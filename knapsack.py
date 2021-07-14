4f82e769262d1a12f723a07306db9882d03d2d89
Fatma Ridaoui <rmham93@gmail.com>
2021-07-14 23:43:37 +0300

    knapsack cozumu
    Knapsack probleminin çözümünü anlattığım youtube videosu için kaynak.

---
+++ knapsack.py
@@ -0,0 +1,19 @@
+def knapsack(W, wt, val, n):
+    K = [[0 for x in range(W + 1)] for x in range(n + 1)]
+ 
+    for i in range(n + 1):
+        for j in range(W + 1):
+            if i == 0 or j == 0:
+                K[i][j] = 0
+            elif wt[i-1] <= j:
+                K[i][j] = max(val[i-1] + K[i-1][j-wt[i-1]], K[i-1][j])
+            else:
+                K[i][j] = K[i-1][j]
+ 
+    return K[n][W]
+ 
+val = [12,10,20,15]
+wt = [2,1,3,2]
+W = 5
+n = len(val)
+print(knapsack(W, wt, val, n))
